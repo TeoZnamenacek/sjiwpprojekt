dbbf82f7471282c47807adf75944c83b8668871d
Teo Znamenaček <teo.znamenacek@ad.tsd.hr>
2025-04-15 11:40:24 +0200

    dodali smo h1 naslov

--- public/index.php
+++ public/index.php
@@ -35,6 +35,7 @@
 
   </head>
   <body>
+    <h1>Namjestaj</h1>
   
     <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
